bde7905ab3df90d98ea0ef9d6d7a806236c08c9e
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-03-17 15:28:04 +0000

    Stunt Car Update

--- multistuntcar/stuntcarracer.js
+++ multistuntcar/stuntcarracer.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\paula\AppData\Local\Temp\tmpymyvgd73.js
+// include: C:\Users\paula\AppData\Local\Temp\tmpsns8eghk.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -207,14 +207,14 @@ Module['FS_createPath']("/data", "Tracks", true, true);
 
   })();
 
-// end include: C:\Users\paula\AppData\Local\Temp\tmpymyvgd73.js
-// include: C:\Users\paula\AppData\Local\Temp\tmpxn79nk2g.js
+// end include: C:\Users\paula\AppData\Local\Temp\tmpsns8eghk.js
+// include: C:\Users\paula\AppData\Local\Temp\tmpi31jpq_a.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\paula\AppData\Local\Temp\tmpxn79nk2g.js
+  // end include: C:\Users\paula\AppData\Local\Temp\tmpi31jpq_a.js
 // include: C:\Users\paula\emsdk\upstream\emscripten\src\emrun_prejs.js
 /**
  * @license
@@ -237,13 +237,13 @@ if (globalThis.window) {
   }
 }
 // end include: C:\Users\paula\emsdk\upstream\emscripten\src\emrun_prejs.js
-// include: C:\Users\paula\AppData\Local\Temp\tmp0w9gzo0f.js
+// include: C:\Users\paula\AppData\Local\Temp\tmp5ctbedlh.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\paula\AppData\Local\Temp\tmp0w9gzo0f.js
+  // end include: C:\Users\paula\AppData\Local\Temp\tmp5ctbedlh.js
 
 
 var arguments_ = [];
